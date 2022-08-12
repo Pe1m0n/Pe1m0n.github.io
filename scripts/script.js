211dc6d1c66f0c4324a182267bc74bff18a72862
Pe1m0n <64415471+Pe1m0n@users.noreply.github.com>
2022-08-12 23:18:19 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+    $('.duckbtn:eq(0)').dblclick(function(){
+        $('body').css("background-color","cyan");
+        $('body .content').css("background-color","#FFFAFA");
+        $('body .content h1,h2').css("color","#fcce26");
+        $('body .content h1,.slider').css("border-color","#fcce26");
+        $('body .content .bar').css("background-color","#fcce26");
+    });
+
+    $('.duckbtn:eq(1)').dblclick(function(){
+        $('body').css("background-color","red");
+        $('body .content').css("background-color","#FFFAFA");
+        $('body .content h1,h2').css("color","#fcce26");
+        $('body .content h1,.slider').css("border-color","#fcce26");
+        $('body .content .bar').css("background-color","#fcce26");
+    });
+
+    $('.duckbtn:eq(2)').dblclick(function(){
+        $('body').css("background-color","green");
+        $('body .content').css("background-color","#FFFAFA");
+        $('body .content h1,h2').css("color","#fcce26");
+        $('body .content h1,.slider').css("border-color","#fcce26");
+        $('body .content .bar').css("background-color","#fcce26");
+    });
+
+    $('.duckbtn:eq(3)').dblclick(function(){
+        $('body').css("background-color","pink");
+        $('body .content').css("background-color","#FFFAFA");
+        $('body .content h1,h2').css("color","#fcce26");
+        $('body .content h1,.slider').css("border-color","#fcce26");
+        $('body .content .bar').css("background-color","#fcce26");
+    });
+
+
+
+});
